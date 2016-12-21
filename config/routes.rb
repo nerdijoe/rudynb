c8927c6e76713a02865b376c1ed24ecf93dbae73
Rudy Wahjudi <rudy.wahjudi@gmail.com>
2016-12-21 17:23:16 +0800

    add static page index

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
+  get 'static/index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
-  root 'application#hello'
+  root 'static#index'
 
 
   # Example of regular route:
