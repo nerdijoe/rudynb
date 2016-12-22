0a9d14b59d858bb6c7bc7c4f0aeff19d9bf1d783
Rudy Wahjudi <rudy.wahjudi@gmail.com>
2016-12-22 17:33:00 +0800

    added clearance, user can signup, login, logout.

---
+++ db/migrate/20161222022929_create_users.rb
@@ -0,0 +1,20 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.timestamps null: false
+      t.string :email, null: false
+      t.string :encrypted_password, limit: 128, null: false
+      t.string :confirmation_token, limit: 128
+      t.string :remember_token, limit: 128, null: false
+
+      t.string :firstname, null: false
+      t.string :lastname, null: false
+      t.string :phone
+      t.string :nationality
+
+    end
+
+    add_index :users, :email
+    add_index :users, :remember_token
+  end
+end
