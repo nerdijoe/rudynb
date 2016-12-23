1ccba57bf6eea51c7c6e085525b287a76c20a846
Rudy Wahjudi <rudy.wahjudi@gmail.com>
2016-12-23 19:00:32 +0800

    omniauth step 6

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,16 @@
 class User < ActiveRecord::Base
   include Clearance::User
-  
+
+  has_many :authentications, :dependent => :destroy
+
+  def self.create_with_auth_and_hash(authentication, auth_hash)
+    user = Authentication.create(firstname: auth_hash["name"], email: auth_hash["extra"]["raw_info"]["email"])
+    user.authentications<<(authentication)
+  end
+
+  def fb_token
+    x = self.authentications.where(:provider => :facebook).first
+    return x.token unless x.nil?
+  end
+
 end
