755a21c7d251a20f1b73099a2160c2042b9d1b94
Rudy Wahjudi <rudy.wahjudi@gmail.com>
2016-12-23 13:25:44 +0800

    added ajax feat for signing up a new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,11 @@ class UsersController < Clearance::UsersController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to sign_in_path
+      respond_to do |format|
+        format.html { redirect_to root_url }
+        format.js
+      end
+      # redirect_to sign_in_path
     else
       render root_path
     end
