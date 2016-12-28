0135cd2d5dd8a7feb36b148bfd25d78bda855248
Rudy Wahjudi <rudy.wahjudi@gmail.com>
2016-12-28 15:42:16 +0800

    move allow method to application_helper, use 'if allow?' instead of 'if user.customer?' in the show.html.erb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,41 +10,6 @@ class ApplicationController < ActionController::Base
     render html: "hello, rudy!"
   end
 
-  def allow?(controller, action)
-    if current_user.customer?
-      # controller == 'listings' && action == 'index'
-      # controller == 'listings' && action.in?(%w[index show new create edit update])
-
-      if controller == 'listings'
-        if action.in?(%w[index show new create])
-          return true
-        elsif action.in?(%w[edit update])
-          # user can only edit his own listing
-          if Listing.find(params[:id]).user_id == current_user.id
-            return true
-          end
-        end
-      end
-
-      false
-
-    elsif current_user.moderator?
-      controller == 'listings' && action.in?(%w[index show new create edit update verify])
-    elsif current_user.superadmin?
-      true
-    else
-      false
-    end
-  end
-
-
-  private
-  def authorize
-    if !allow?(params[:controller], params[:action])
-      redirect_to listing_path(params[:id]), alert: "Not authorized"
-    end
-  end
-
 
 
 
