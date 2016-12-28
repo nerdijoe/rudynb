aeeaed6729aa76cb88c95c01352a7c7a639a9108
Rudy Wahjudi <rudy.wahjudi@gmail.com>
2016-12-28 11:00:53 +0800

    managed to give forbid cuistomer to listings edit action

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,4 +1,5 @@
 class ListingsController < ApplicationController
+  before_action :authorize, only: :edit
 
   def index
     if params[:tag]
@@ -51,9 +52,17 @@ class ListingsController < ApplicationController
   end
 
 
+
+
   private
   def listing_params
     params.require(:listing).permit(:title, :description, :address, :country, :phone, :num_bedrooms, :price, :currency, :house_rules, :tag_list)
+  end
+
+  def authorize
+    if current_user.customer?
+      redirect_to listings_path, alert: "Not authorized"
+    end
   end
 
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161228015156) do
+ActiveRecord::Schema.define(version: 20161228020017) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -73,18 +73,19 @@ ActiveRecord::Schema.define(version: 20161228015156) do
   add_index "tags", ["name"], name: "index_tags_on_name", unique: true, using: :btree
 
   create_table "users", force: :cascade do |t|
-    t.datetime "created_at",                     null: false
-    t.datetime "updated_at",                     null: false
-    t.string   "email",                          null: false
+    t.datetime "created_at",                                 null: false
+    t.datetime "updated_at",                                 null: false
+    t.string   "email",                                      null: false
     t.string   "encrypted_password", limit: 128
     t.string   "confirmation_token", limit: 128
-    t.string   "remember_token",     limit: 128, null: false
-    t.string   "firstname",                      null: false
-    t.string   "lastname",                       null: false
+    t.string   "remember_token",     limit: 128,             null: false
+    t.string   "firstname",                                  null: false
+    t.string   "lastname",                                   null: false
     t.string   "phone"
     t.string   "nationality"
     t.string   "profile_pic"
     t.integer  "age"
+    t.integer  "role",                           default: 0, null: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", using: :btree
