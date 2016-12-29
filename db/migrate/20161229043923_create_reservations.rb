4883f003a970cf0da303d3ef3c236e4660d60305
Rudy Wahjudi <rudy.wahjudi@gmail.com>
2016-12-29 12:47:59 +0800

    revise CreateReservations

--- db/migrate/20161229043923_create_reservations.rb
+++ db/migrate/20161229043923_create_reservations.rb
@@ -1,10 +1,10 @@
 class CreateReservations < ActiveRecord::Migration
   def change
     create_table :reservations do |t|
-      t.belongs_to, :user
-      t.belongs_to, :listing
-      t.date, :start_date
-      t.date, :end_date
+      t.belongs_to :user, index: true, foreign_key: true
+      t.belongs_to :listing, index: true, foreign_key: true
+      t.date :start_date
+      t.date :end_date
       t.integer :num_guests
 
       t.timestamps null: false
